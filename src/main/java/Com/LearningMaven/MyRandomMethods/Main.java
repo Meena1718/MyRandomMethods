268b1aefdabd190675ec30cda9a5967d80a67b3e
Meena Kabduwal <kabduwalmeena@gmail.com>
2021-03-18 00:03:53 +0100

    Project completed

--- src/main/java/Com/LearningMaven/MyRandomMethods/Main.java
+++ src/main/java/Com/LearningMaven/MyRandomMethods/Main.java
@@ -3,8 +3,29 @@ package Com.LearningMaven.MyRandomMethods;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		MyRandomMethods mrm = new MyRandomMethods();
+		
+		for(int i = 0;i < 30 ;i++) {
+		System.out.print(mrm.randomBetween1And10()+", ");
+	}   
+		System.out.println();
+		
+		for(int i = 0;i < 30 ;i++) {
+			System.out.print(mrm.randomBetween11And20()+", ");
+		}
+		System.out.println();
+		for(int i = 0;i < 30 ;i++) {
+			System.out.print(mrm.randomBetween21And30()+", ");
+		}
+		System.out.println();
+		
+		for(int i = 0;i < 30 ;i++) {
+			System.out.print(mrm.randomBetween31And40()+", ");
+		}
+		System.out.println();
+		for(int i = 0;i < 30 ;i++) {
+			System.out.print(mrm.randomBetween41And50()+", ");
+		}
 	}
 
 }
